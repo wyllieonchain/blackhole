ca6d7c9e23cbc147009633fe2d7b475485fd1c72
malcolm21 <malcolm@buzzmarkets.io>
2025-03-17 19:29:02 -0400

    analytics fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
